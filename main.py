b8d24a9fcf9b40d2b93c53ea19e326242dcb8365
yang837 <1535270292@qq.com>
2024-04-11 18:09:20 +0800

    feat(*):程序流程

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
-from netmiko import ConnectHandler
+from netmiko import ConnLogOnly
+from threading import Thread
 
 
 # ip username password
 def get_info() -> list:
-    info = []
+    info = ["ip", "user", "pwd"]
     return info
 
 
 # cmd
 def get_command() -> list:
-    cmds = []
-    return cmds
+    commands = []
+    try:
+        with open("config/config.txt") as f:
+            commands = f.readlines()
+    except Exception as e:
+        print(e)
+    return commands
 
 
 # ping
@@ -21,28 +27,47 @@ def alive(ip) -> bool:
 
 
 # ssh telnet
-def login(ip, username, password) -> ConnectHandler():
-    device = {
+def login(ip, username, password):
+    device_ssh = {
         'device_type': 'huawei',
         'ip': ip,
         'username': username,
         'password': password,
         'port': 22
     }
-    with ConnectHandler(**device) as con:
-        return con
+    device_telnet = {
+        'device_type': 'huawei',
+        'ip': ip,
+        'username': username,
+        'password': password,
+        'port': 21
+    }
+    try:
+        with ConnLogOnly(**device_ssh) as con_ssh:
+            if con_ssh is None:
+                with ConnLogOnly(**device_telnet) as con_telnet:
+                    return con_telnet
+            else:
+                return con_ssh
+    except Exception as e:
+        print(e)
 
 
 # send conf
-def config(con, command):
-    for cmd in command:
-        con.send_config_set(cmd)
+def config(info, command):
+    if alive(info[0]):
+        con = login(info[0], info[1], info[2])
+        print(con)
+        for cmd in command:
+            con.send_config_set(cmd)
+    else:
+        print("offline")
 
 
 if __name__ == '__main__':
     device_info = get_info()
-    commands = get_command()
+    command = get_command()
     for info in device_info:
-        if alive(info[0]):
-            con = login(info[0], info[1], info[2])
-            config(con, commands)
+        t = Thread(target=config, args=(device_info, command))
+        t.start()
+        t.join()
